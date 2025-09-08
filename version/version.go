2d9e511c6eed2fcd203752b2410b2eff8eeb190b
Martin Buchleitner <martin@nitram.at>
2025-09-08 07:07:06 +0200

    fix: update ci workflow (#21)
    * feat: enhance CI to update all version constants on main branch builds
    
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>
    
    * docs: comprehensive README update for v1.3.0
    
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>
    
    * fix: remove go list on test
    
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>
    
    * fix: correct ldflags
    
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>
    
    * fix: version config
    
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>
    
    ---------
    
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- version/version.go
+++ version/version.go
@@ -2,17 +2,14 @@ package version
 
 import "fmt"
 
-const (
-	Name      string = "imap"
-	Version   string = "1.3.1"
-	GitCommit string = "25e39e5c11229e7a5bd09b7d114c7ac4d5ce6469"
-	GoVersion string = "1.24.6"
-	BuildDate string = "2025-09-05T14:07:14Z"
-	GitDirty  string = ""
-)
-
 var (
-	PluginVersion = fmt.Sprintf("%s-%s", Version, GitDirty)
+	Name      string
+	Version   string
+	GitCommit string
+	GoVersion string
+	BuildDate string
+	GitDirty  string
 
-	HumanVersion = fmt.Sprintf("%s v%s (%s) %s", Name, Version, GitCommit, GitDirty)
+	PluginVersion = fmt.Sprintf("%s%s", Version, GitDirty)
+	HumanVersion  = fmt.Sprintf("%s v%s (%s) %s", Name, Version, GitCommit, GitDirty)
 )
